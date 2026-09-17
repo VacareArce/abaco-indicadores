f162c8e63bbb40ee45382152a629f3b4907e38bd
VacareArce <estebanmonsalverojas@gmail.com>
2026-09-16 19:59:13 -0500

    Actualizar enlaces de fichas técnicas del inventario

--- js/menu_lista_tableros.js
+++ js/menu_lista_tableros.js
@@ -1,126 +1,126 @@
 function lista_tableros(){
 	var tableros={
 				"gini": 	{ "tablero":"p_c5eh27ha4c", 
-							  "ficha":"https://drive.google.com/file/d/1HAeQd1BeUBcXbxBbFmxjCkqLMQj0duSZ/view?usp=drive_link"
+							  "ficha":"https://drive.google.com/file/d/1-4qNYarHJywkX9JrIBYESaX6-BfIfP1A/view?usp=drive_link"
 							},
 				"Pobreza_Monetaria_Mun_BQ": { "tablero":"",
-							   "ficha":"https://drive.google.com/file/d/1hsSN6FPEE9i0nfVC3NtICtJJvVNs161V/view?usp=drive_link"
+							   "ficha":"https://drive.google.com/file/d/1-FBp6aSVk7AHbAVm7l-ozlxx-VYuQzNr/view?usp=drive_link"
 							},	
 				"pme":  	{ "tablero":"p_a0l14xha4c", 
-						       "ficha":"https://drive.google.com/file/d/1i3SoMmMjG_aIsqWv6Mu8p9FtqpDMradq/view?usp=drive_link"
+						       "ficha":"https://drive.google.com/file/d/15vgKh7RSwApv3eNoFpeUhefTzigaxWoD/view?usp=drive_link"
 							},     
 				"IPMultidimensional_Mun_BQ": { "tablero":"",
-							       "ficha":"https://drive.google.com/file/d/1hsFDyTKx1S19z2i3LJrqeNI3-j56BQoN/view?usp=drive_link"
+							       "ficha":"https://drive.google.com/file/d/1qj9pujoT5MUe1wezok27iUTGk7rDJgfq/view?usp=drive_link"
 							},
 				"desempleo":{ "tablero":"p_iry9huom4c", 
-						      "ficha":"https://drive.google.com/file/d/1iOfatRe71iQnGHLj5pt12oOCVxGH076P/view"
+						      "ficha":"https://drive.google.com/file/d/1m0asHQqpeiguO3JTFUTElPl_SYSCfXhg/view?usp=drive_link"
 						    },
 				"acueducto":{ "tablero":"p_yphe8nlm4c", 
-						 "ficha":"https://drive.google.com/file/d/1InYP7TXk7ZqebB6T4G7SaQg2yI0O1B92/view?usp=drive_link"
+						 "ficha":"https://drive.google.com/file/d/1jSdlSQk8SPYMh5LaybaFpMx6PbSum-AU/view?usp=drive_link"
 							},
 				"alcantarillado": { "tablero":"p_kswaynqm4c", 
-									"ficha":"https://drive.google.com/file/d/1-kXI6-eZL4oi26oypuA9nTJ_gtqSS72V/view?usp=drive_link"
+									"ficha":"https://drive.google.com/file/d/1VOZEp6QzxzJNYyBKe5vZ3c6jfdA0VrYg/view?usp=drive_link"
 						    },
 				"bpNacer":	{ "tablero":"p_zjhb0szs6c", 
-							  "ficha":"https://drive.google.com/file/d/1hKQztS0ssVp1oN_Wq8d3JL9fsayyVFHy/view?usp=sharing"
+							  "ficha":"https://drive.google.com/file/d/1Qs1kJsN2xe9Oje_K9TyV5dbb8TEw49fc/view?usp=drive_link"
 							},
 				"mortDNT":	{ "tablero":"p_e7fnlil5gd", 
-							  "ficha":"https://drive.google.com/file/d/1rPpWn5fnh3Gt62zEtRxUaDeMKTz-DwKM/view?usp=drive_link"
+							  "ficha":"https://drive.google.com/file/d/1Y3cw6nuT6fJ-nLEtekGTu2V9qtAelroZ/view?usp=drive_link"
 							},
 				"pob04":	{ "tablero":"p_e7yq71q94c", 
 							  "ficha":"https://drive.google.com/file/d/1o0TVWZiHtkYzb-lDPZFexnp34YEf5Q92/view"
 							},
 				"proyeccion":{ "tablero":"p_tnmcpbod6c", 
-							   "ficha":"https://drive.google.com/file/d/1hfZUsxkGMi9z8kW7LkbQT6WDS6xUcQhN/view?usp=drive_link"
+							   "ficha":"https://drive.google.com/file/d/1MB1ghOsXbss527NQ8C-5Vb0DMDyjEO8x/view?usp=drive_link"
 							 },
 				"proyeccionCM":{ "tablero":"p_nw5r2itb7c", 
-								  "ficha":"https://drive.google.com/file/d/1he7UTS0rUNv4-_S_BU0TaIMmgvbt-qfN/view?usp=drive_link"
+								  "ficha":"https://drive.google.com/file/d/1aClIbYdGcqEVf72-Swkv18bOmmVXOyl7/view?usp=drive_link"
 							},
 				"proyeccionCPyRD":{ "tablero":"p_5uezzy7d7c", 
-									 "ficha":"https://drive.google.com/file/d/1h_mnyqyVeZEzkNDG7bI3G95sW_hyOfKB/view?usp=drive_link"
+									 "ficha":"https://drive.google.com/file/d/1AnfDXE_Qnq43EkT-88XhDZZhMpB-r368/view?usp=drive_link"
 							},
 				"variacionPob":{"tablero":"p_iwl7nmqv7c", 
 								"ficha":"https://drive.google.com/file/d/1o0TVWZiHtkYzb-lDPZFexnp34YEf5Q92/view"
 								},
 				"retTalla":		{ "tablero":"p_gwt6umta4c", 
-								   "ficha":"https://drive.google.com/file/d/1HHUPEMwDNRLZyeU5hcNdkEqY5mSuVMao/view?usp=drive_link"
+								   "ficha":"https://drive.google.com/file/d/1nFL5Yf9ugsZ-J6RgW-ad9uTmyoLMeyH_/view?usp=drive_link"
 							    },
 				"exePeso":		{ "tablero":"p_a351uvta4c", 
-								  "ficha":"https://drive.google.com/file/d/1HHDg36KU8YV1y-Iq0GcmoKPxqbjfCDqg/view?usp=drive_link"
+								  "ficha":"https://drive.google.com/file/d/1KN9-TMyBWdwYuxlnFwqPA15KI2Fm3Xmu/view?usp=drive_link"
 								},
 				"anemia":		{ "tablero":"p_t9gueh4a5c", 
-								   "ficha":"https://drive.google.com/file/d/1HBxBft9GoadfyLv3wpfBBTfFfFXeNgPh/view?usp=drive_link"
+								   "ficha":"https://drive.google.com/file/d/1a_nRamwk77EKO_EpV3OCzXlDX2vvJCXC/view?usp=drive_link"
 							    },
 				"hierro":		{ "tablero":"p_t5nkv93a5c", 
-								   "ficha":"https://drive.google.com/file/d/1H6O8yqtke0JBZdHPPrOSVcdhnsNVRyj1/view?usp=drive_link"
+								   "ficha":"https://drive.google.com/file/d/1HEMZcArTYb_ku13CYCxZPqjd4rqwsKtD/view?usp=drive_link"
 							    },
 				"vitaminaA":	{ "tablero":"p_336zf14a5c", 
-								  "ficha":"https://drive.google.com/file/d/1H-FnFPog6xNcj50btBCs-Msonj9qYdiX/view?usp=drive_link"
+								  "ficha":"https://drive.google.com/file/d/18vpJOX9JDW3grVkFcKDlEYClMgqHTj9W/view?usp=drive_link"
 								},
 				"zinc":			{ "tablero":"p_kliej44a5c", 
-								  "ficha":"https://drive.google.com/file/d/1Gy9_U5Z2ZnUkbJMKgAPTIUwsaew7iwK0/view?usp=drive_link"
+								  "ficha":"https://drive.google.com/file/d/1W4ZpYlUHpHlL0WjoulRlRIQunAdYgPwF/view?usp=drive_link"
 								},
 				"InsAlimentaria":{ "tablero":"p_xwuwxy665c", 
 									"ficha":"https://drive.google.com/file/d/1o0TVWZiHtkYzb-lDPZFexnp34YEf5Q92/view"
 								},
-				"Ins_Alimentaria_Mun_22_BQ": {"tablero":"", "ficha":"https://drive.google.com/file/d/1Gy9_U5Z2ZnUkbJMKgAPTIUwsaew7iwK0/view?usp=drive_link"},
+				"Ins_Alimentaria_Mun_22_BQ": {"tablero":"", "ficha":"https://drive.google.com/file/d/1Y-Hvwn3CpGyFw7uo_iq6GQXS3yCHv7c3/view?usp=drive_link"},
 				"InsAlimentariaM":	{ "tablero":"p_oe9ziedgdd", 
-									  "ficha":"https://drive.google.com/file/d/1GtOjyk4M7q6nXquvxnMHjS9sBtpkxVqu/view?usp=drive_link"
+									  "ficha":"https://drive.google.com/file/d/1mzs1zAKCnTtWOsR51KPxCc7vBBLWcuYW/view?usp=drive_link"
 								    },
 				"NacPCalificado":   {  "tablero":"p_ho4gdc61dd", 
-									   "ficha":"https://drive.google.com/file/d/1hGVcru4mhTN_GikY-cyKPG7YmJrBXxfn/view?usp=drive_link"
+									   "ficha":"https://drive.google.com/file/d/1rgNYZZ9uj872OAoIRNWq_Dzzw2RLv_Sf/view?usp=drive_link"
 									},
 				"PINM5":   {  "tablero":"p_etn73wk4md", 
-									   "ficha":"https://drive.google.com/file/d/1YsOhLLsn_lRDzByF0jjbXHIr2OrdQ2cj/view?usp=drive_link"
+									   "ficha":"https://drive.google.com/file/d/1i-5vg9Z9fLI3Hw7EcqZVoZXnzqAk9dmf/view?usp=drive_link"
 									},
 				"mortM1":			{"tablero":"p_g7w9uai2dd", 
-									 "ficha":"https://drive.google.com/file/d/1pU3z2Ey3Q4um3k7gscgEnb5LxtzRSwMO/view?usp=drive_link"
+									 "ficha":"https://drive.google.com/file/d/1ZOPub2L7vhhP0Yx6EotZSLow0sH-mgjp/view?usp=drive_link"
 									},
 				"mortM5":			{ "tablero":"p_fj3csn8ogd", 
-									  "ficha":"https://drive.google.com/file/d/1pgiuVaDeaWfiagCPk6M5RVHM3BcujHQQ/view?usp=drive_link"
+									  "ficha":"https://drive.google.com/file/d/1jJPYwTQCD-srIvuJ71HFVunROPYfyvcZ/view?usp=drive_link"
 									},
 				"TNat":				{"tablero":"p_zosh3ethed", 
-									 "ficha":"https://drive.google.com/file/d/1hFOzEtzCzUk07BOe4sqhNTXwdS9HUPoH/view?usp=drive_link"
+									 "ficha":"https://drive.google.com/file/d/1rIVPJLY2scIAqToHfI3kjMbdmg_wG-rd/view?usp=drive_link"
 									},
 				"TNatM14":			{"tablero":"p_6q5es8myfd", 
-									 "ficha":"https://drive.google.com/file/d/1kyMZaBccRLRgUUHE3BLITmTfiVBNkBWA/view?usp=drive_link"
+									 "ficha":"https://drive.google.com/file/d/1vWX0ZdoMnu7EJpvLu3_0FqL-DPJ-oNx6/view?usp=drive_link"
 									},
 				"TNatM19":			{"tablero":"p_nvsmhtxeid", 
-									 "ficha":"https://drive.google.com/file/d/1TBcGnNQ1iP9dridXvXw-jfgDXRYrHsYI/view?usp=drive_link"
+									 "ficha":"https://drive.google.com/file/d/1rS0NKQMK0NuafM8GespgFYoLS5SnEEGt/view?usp=drive_link"
 									},
 				"TGF":		    	{ "tablero":"p_xd7n5piknd", 
-									  "ficha":"https://drive.google.com/file/d/1vtc-Ymw-ZcyPLzo1deWrgNFAbuFxNUwz/view?usp=drive_link"
+									  "ficha":"https://drive.google.com/file/d/1sI0lwqFVzfTrQpC3AIwHyqiH1Nytuc0_/view?usp=drive_link"
 									},
 								
 				"ContrelesPreNat":	{"tablero":"p_0dnp4ioied", 
-									 "ficha":"https://drive.google.com/file/d/1hE_HmgQGmQfZuxvHD6qHJk1HnRnb8c48/view?usp=drive_link"
+									 "ficha":"https://drive.google.com/file/d/1_p2uqvgv0xc1B8bDGklgV8LprG2s9Syy/view?usp=drive_link"
 									},
 				"MortIRA":			{"tablero":"p_kb9jy0d4gd", 
-									 "ficha":"https://drive.google.com/file/d/1j99q0sKv-uWQpxzW75B1Tdgujm4xXG8g/view?usp=drive_link"
+									 "ficha":"https://drive.google.com/file/d/1GZL2kazOod19EshO-WUrfwHr73zFhPL2/view?usp=drive_link"
 									},
 				"MortEDA":			{"tablero":"p_gwelz8zejd", 
-									  "ficha":"https://drive.google.com/file/d/1Lbl2Db_tNmMABudqH002pRhHr9alz4zv/view?usp=drive_link"
+									  "ficha":"https://drive.google.com/file/d/1Hub2DgXsFPtmO0YG460o34qeVKevc7Ly/view?usp=drive_link"
 					},
 				"MortMAT":			{"tablero":"p_wgz0pvrxjd", 
-									  "ficha":"https://drive.google.com/file/d/1hDQWzstw1avknHw3UrSLAnDAQD0EuCLt/view?usp=drive_link"
+									  "ficha":"https://drive.google.com/file/d/1wYJP4thEZu38IjV18eR7R45BPomNt-__/view?usp=drive_link"
 					},
 				"PDA":			{"tablero":"p_boz6i686kd", 
-									"ficha":"https://drive.google.com/file/d/1_PgWZ07CRl0Ca571QPj4ab4wl6g3UUF-/view"
+									"ficha":"https://drive.google.com/file/d/1UKeF5ilkI0NVj3XCm6c4QXIJ1eAJQhXC/view?usp=drive_link"
 				},
 				"NEPrimaria":			{"tablero":"p_ui21z2gykd", 
-									"ficha":"https://drive.google.com/file/d/1avWNA-SxZTMnK4-fiD9-nKpbGKme3RhS/view"
+									"ficha":"https://drive.google.com/file/d/19VKjHO1Wjzp1pbF1HjNE6aY2NKl1c0x-/view?usp=drive_link"
 				},
 				"NEScundaria":			{"tablero":"p_59y8j5gykd", 
-									"ficha":"https://drive.google.com/file/d/1avekkNb76f5w31uGWqPI5O0iRgXm-MoV/view"
+									"ficha":"https://drive.google.com/file/d/1DDA14tP-jWXQ8g1bDQYhtS3TqFioSbpF/view?usp=drive_link"
 				},
 				"NESuperior":			{"tablero":"p_voa7m5gykd", 
-									"ficha":"https://drive.google.com/file/d/1ayllD7VgHOGObFe4ZOXwj0NiJ1apfo7k/view"
+									"ficha":"https://drive.google.com/file/d/1MU6g-gdtHo_tkrA4v3A1Yh9GBCvXVQJf/view?usp=drive_link"
 				},
 				"IRCA":			{"tablero":"p_cym27g2opd", 
-										"ficha":"https://drive.google.com/file/d/1Xp3g_OxdeuZ6oASIoRyt4ZPVa9yZNskW/view"
+										"ficha":"https://drive.google.com/file/d/10tkXWNDTnTJX9GEA1HQ8AZmnxqXJM8c5/view?usp=drive_link"
 				},
 				"PINWASHM5":			{"tablero":"p_m5neaswpqd", 
-										"ficha":"https://drive.google.com/file/d/1ZigwF91akUEpaNeK3jXEgjNa_9WRnttL/view?usp=drive_link"
+										"ficha":"https://drive.google.com/file/d/1Xo_Rx9pQTJnatO9eoDvCD55zYVXdg84p/view?usp=drive_link"
 				},
 				"IPUF":					{"tablero":"p_frp0ts7jqd", 
 										"ficha":"https://drive.google.com/file/d/1Lmzpj5Fez-Z7prDCGns2Qigh1ySYxvr0/view?usp=drive_link"
@@ -128,33 +128,32 @@ function lista_tableros(){
 				"IPUF_VOL":					{"tablero":"p_vfn1nub8qd", 
 											  "ficha":"https://drive.google.com/file/d/1Df62ZHteBJX0pS2nSyaEn0_zlqxE6qJX/view?usp=drive_link"
 				},
-				"T_Verduras_BQ":			{"tablero":"p_c7z0yosg1d", "ficha":"https://drive.google.com/file/d/1YJukpvbYnYcsBiWcN1kQTyLgWyNSwFvr/view?usp=drive_link"},
-				"T_Legumbres_BQ":			{"tablero":"p_83tywwsg1d", "ficha":"https://drive.google.com/file/d/1YJukpvbYnYcsBiWcN1kQTyLgWyNSwFvr/view?usp=drive_link"},
-				"T_Cereales_BQ":			{"tablero":"p_ofjkyysg1d", "ficha":"https://drive.google.com/file/d/1YJukpvbYnYcsBiWcN1kQTyLgWyNSwFvr/view?usp=drive_link"},
-				"T_Frutas_BQ":				{"tablero":"p_5mbwhblh1d", "ficha":"https://drive.google.com/file/d/1YJukpvbYnYcsBiWcN1kQTyLgWyNSwFvr/view?usp=drive_link"},
-				"T_Productos_lacteos_BQ":	{"tablero":"p_ebnwgflh1d", "ficha":"https://drive.google.com/file/d/1YJukpvbYnYcsBiWcN1kQTyLgWyNSwFvr/view?usp=drive_link"},
-				"T_Productos_carnicos_BQ":	{"tablero":"p_jr7h5hlh1d", "ficha":"https://drive.google.com/file/d/1YJukpvbYnYcsBiWcN1kQTyLgWyNSwFvr/view?usp=drive_link"},
-				"T_Raices_tuberculos_y_platanos_BQ": {"tablero":"p_of0ewslh1d", "ficha":"https://drive.google.com/file/d/1YJukpvbYnYcsBiWcN1kQTyLgWyNSwFvr/view?usp=drive_link"},
-				"T_Huevos_BQ":				{"tablero":"p_cs3g2wlh1d", "ficha":"https://drive.google.com/file/d/1YJukpvbYnYcsBiWcN1kQTyLgWyNSwFvr/view?usp=drive_link"},
-				"RT_Compra_mas_BQ":			{"tablero":"p_bnb1xhqg1d", "ficha":"https://drive.google.com/file/d/1j5vqaMqc63txhpRXxlrTLoaE-pj6uQtt/view?usp=drive_link"},
-				"RT_Prepara_mas_BQ":		{"tablero":"p_wqcfpxrg1d", "ficha":"https://drive.google.com/file/d/1j5vqaMqc63txhpRXxlrTLoaE-pj6uQtt/view?usp=drive_link"},
-				"RT_Humedad_temperatura_BQ":{"tablero":"p_fwuno1rg1d", "ficha":"https://drive.google.com/file/d/1j5vqaMqc63txhpRXxlrTLoaE-pj6uQtt/view?usp=drive_link"},
-				"RT_Mala_conservacion_BQ":	{"tablero":"p_mm3bg8rg1d", "ficha":"https://drive.google.com/file/d/1j5vqaMqc63txhpRXxlrTLoaE-pj6uQtt/view?usp=drive_link"},
-				"RT_Vencimiento_BQ":		{"tablero":"p_i1dygdsg1d", "ficha":"https://drive.google.com/file/d/1j5vqaMqc63txhpRXxlrTLoaE-pj6uQtt/view?usp=drive_link"},
-				"RT_Falta_refrigeracion_BQ":{"tablero":"p_4oaynfsg1d", "ficha":"https://drive.google.com/file/d/1j5vqaMqc63txhpRXxlrTLoaE-pj6uQtt/view?usp=drive_link"},
-				"RT_Exceso_tiempo_BQ":		{"tablero":"p_sr705lsg1d", "ficha":"https://drive.google.com/file/d/1j5vqaMqc63txhpRXxlrTLoaE-pj6uQtt/view?usp=drive_link"},
+				"T_Verduras_BQ":			{"tablero":"p_c7z0yosg1d", "ficha":"https://drive.google.com/file/d/1_-eAgXmqjzzr7M6z_xy0Ozz8JjIgR1bO/view?usp=drive_link"},
+				"T_Legumbres_BQ":			{"tablero":"p_83tywwsg1d", "ficha":"https://drive.google.com/file/d/1WjlTFRNeo5BVEn_ZqZm0TIiLMOjJNEiv/view?usp=drive_link"},
+				"T_Cereales_BQ":			{"tablero":"p_ofjkyysg1d", "ficha":"https://drive.google.com/file/d/1MKUHHmYMIOB9Dw-i3RPyv-2U4hQ9TTcM/view?usp=drive_link"},
+				"T_Frutas_BQ":				{"tablero":"p_5mbwhblh1d", "ficha":"https://drive.google.com/file/d/18hdP4TVbhh-yHi1OeqQRUX7JSyfd5BZM/view?usp=drive_link"},
+				"T_Productos_lacteos_BQ":	{"tablero":"p_ebnwgflh1d", "ficha":"https://drive.google.com/file/d/1l82-jpVlOzwidFHsTORJ8BAvw1_9rMIb/view?usp=drive_link"},
+				"T_Productos_carnicos_BQ":	{"tablero":"p_jr7h5hlh1d", "ficha":"https://drive.google.com/file/d/1UfdRkLbH50_7MCB0HunmYf4MAWGLVWxO/view?usp=drive_link"},
+				"T_Raices_tuberculos_y_platanos_BQ": {"tablero":"p_of0ewslh1d", "ficha":"https://drive.google.com/file/d/1UWTeLTrcNHjB2ovKx5TR8PYyZrALJVvC/view?usp=drive_link"},
+				"T_Huevos_BQ":				{"tablero":"p_cs3g2wlh1d", "ficha":"https://drive.google.com/file/d/15Ww5x9B-dBOblvcdkwPvjYgt4RzEqmON/view?usp=drive_link"},
+				"RT_Compra_mas_BQ":			{"tablero":"p_bnb1xhqg1d", "ficha":"https://drive.google.com/file/d/1s1PapJfEvpt51GFdJywHrtBWk9UuB8st/view?usp=drive_link"},
+				"RT_Prepara_mas_BQ":		{"tablero":"p_wqcfpxrg1d", "ficha":"https://drive.google.com/file/d/1aYljm8262NlZ46HZoYjN4aSf0o9UVNID/view?usp=drive_link"},
+				"RT_Humedad_temperatura_BQ":{"tablero":"p_fwuno1rg1d", "ficha":"https://drive.google.com/file/d/1wlCKMvGubDQzXZaxUaJGs1jz_MmVYp9m/view?usp=drive_link"},
+				"RT_Mala_conservacion_BQ":	{"tablero":"p_mm3bg8rg1d", "ficha":"https://drive.google.com/file/d/1TQARv29r2TvPYI9vNk7fc_g48vzKBsU3/view?usp=drive_link"},
+				"RT_Vencimiento_BQ":		{"tablero":"p_i1dygdsg1d", "ficha":"https://drive.google.com/file/d/1eBRYuC9l_9NaCp8DSUdH59u6JxN1_A8J/view?usp=drive_link"},
+				"RT_Falta_refrigeracion_BQ":{"tablero":"p_4oaynfsg1d", "ficha":"https://drive.google.com/file/d/1zsaeMb4S-YwcKEk_1dxMa6CpL6d5DNka/view?usp=drive_link"},
+				"RT_Exceso_tiempo_BQ":		{"tablero":"p_sr705lsg1d", "ficha":"https://drive.google.com/file/d/1HxB4ahHsffXhKiejT_VhLOAitPTwI3GE/view?usp=drive_link"},
 
-				// Indicadores nuevos (BigQuery nativo). Sin ficha tecnica todavia:
-				// las entrega el Observatorio. 'tablero' va vacio porque la ruta _BQ
+				// Indicadores BigQuery nativos: 'tablero' va vacio porque la ruta _BQ
 				// no pasa por Looker Studio.
-				"Frontera_Agricola_BQ":		{"tablero":"", "ficha":""},
-				"Bosque_Natural_BQ":		{"tablero":"", "ficha":""},
-				"Cambio_Bosque_BQ":			{"tablero":"", "ficha":""},
-				"Tasa_Deforestacion_BQ":	{"tablero":"", "ficha":""},
-				"Erosion_Suelos_BQ":		{"tablero":"", "ficha":""},
-				"Desertificacion_BQ":		{"tablero":"", "ficha":""},
-				"Agro_Familiar_BQ":			{"tablero":"", "ficha":""},
-				"Empleo_Agro_BQ":			{"tablero":"", "ficha":""}
+				"Frontera_Agricola_BQ":		{"tablero":"", "ficha":"https://drive.google.com/file/d/1K0cTbYUzyVmCBNPs7DgOaTXvpVV0Gg3-/view?usp=drive_link"},
+				"Bosque_Natural_BQ":		{"tablero":"", "ficha":"https://drive.google.com/file/d/1juSwCzn7zCYP2ZmUCHNFm2mM30_RDeCU/view?usp=drive_link"},
+				"Cambio_Bosque_BQ":			{"tablero":"", "ficha":"https://drive.google.com/file/d/1yo5wYiiofXtYUSvsbIoM37a4OwqOn4sJ/view?usp=drive_link"},
+				"Tasa_Deforestacion_BQ":	{"tablero":"", "ficha":"https://drive.google.com/file/d/1gGgFjOYvllrpeRESFGbisnCkCtgKueJZ/view?usp=drive_link"},
+				"Erosion_Suelos_BQ":		{"tablero":"", "ficha":"https://drive.google.com/file/d/168S9IV5eZyvpQbVdjXhjg9u-Z3VDtp5c/view?usp=drive_link"},
+				"Desertificacion_BQ":		{"tablero":"", "ficha":"https://drive.google.com/file/d/1nO_tyDU2RzsC1G6aFNq6UID07DZ3fRvE/view?usp=drive_link"},
+				"Agro_Familiar_BQ":			{"tablero":"", "ficha":"https://drive.google.com/file/d/1benYGiANLulXswc7che4JCWTOoD9RXvf/view?usp=drive_link"},
+				"Empleo_Agro_BQ":			{"tablero":"", "ficha":"https://drive.google.com/file/d/1WLicnxYX-u7vlR23YDcxm44PSuhBkVC2/view?usp=drive_link"}
 			};
 	return tableros;
 } 
